b803ea584ed9d6b2f8cb8cd84896f9a06d65deb3
Aimene-BAHRI <bahri.aimen48@gmail.com>
2017-10-13 03:38:31 +0100

    add create event.ejs file

--- models/EventModel.js
+++ models/EventModel.js
@@ -1,6 +1,22 @@
 var mongoose = require('mongoose');
 var Schema   = mongoose.Schema;
 
-var EventSchema = new Schema({	's_date' : Date,	'end_date' : Date,	'place_map' : String,	'adress' : String,	'wilaya' : String,	'needs' : Array,	'participent' : [{	 	type: Schema.Types.ObjectId,	 	ref: 'pending'	}],	'pendingParticipents' : [{	 	type: Schema.Types.ObjectId,	 	ref: 'pending'	}]});
+var EventSchema = new Schema({
+	'name': String,
+	's_date' : Date,
+	'end_date' : Date,
+	'place_map' : String,
+	'adress' : String,
+	'wilaya' : String,
+	'needs' : Array,
+	'participent' : [{
+	 	type: Schema.Types.ObjectId,
+	 	ref: 'pending'
+	}],
+	'pendingParticipents' : [{
+	 	type: Schema.Types.ObjectId,
+	 	ref: 'pending'
+	}]
+});
 
 module.exports = mongoose.model('Event', EventSchema);
--- models/UserModel.js
+++ models/UserModel.js
@@ -1,7 +1,30 @@
 var mongoose = require('mongoose');
 var bcrypt = require('bcrypt-nodejs');
 
-var UserSchema = mongoose.Schema({  profile: {    name: {type: String, default: ""},    pictureUrl: {type: String, default: ""},    // TODO: Add more profile atrributes here  },  local            : {        email        : String,        password     : String,    },    facebook         : {        id           : String,        token        : String,        email        : String,    },    twitter          : {        id           : String,        token        : String,        username     : String    }});
+var UserSchema = mongoose.Schema({
+  profile: {
+    name: {type: String, default: ""},
+    pictureUrl: {type: String, default: ""},
+    phone: {type: String, default: ""},
+    wilaya: {type: String, default: ""},
+    // TODO: Add more profile atrributes here
+  },
+  reputation       : {type: Number, default: 0},
+  local            : {
+        email        : String,
+        password     : String,
+    },
+    facebook         : {
+        id           : String,
+        token        : String,
+        email        : String,
+    },
+    twitter          : {
+        id           : String,
+        token        : String,
+        username     : String
+    }
+});
 
 // generating a hash
 UserSchema.methods.generateHash = function(password) {
